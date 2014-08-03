bcd1bea04e3e3a1e19a7281336f32910ff78bc45
Harrison Brown (MacBook Pro) <harrisonbro@gmail.com>
2014-08-03 07:05:41 +0100

    finalise js file

--- js/src/app.js
+++ js/src/app.js
@@ -3,6 +3,11 @@ var _ = require('underscore'),
 
 //  $ = require('jquery/dist/jquery')(window) for older versions that 2.1.0
 
-var mytemplate = _.template('<h1> <%= name %> </h1>');
+var mytemplate = _.template('<h1> Hello <%= name %> </h1>');
 
-$('body').append(mytemplate({name: 'Harrison'}));
+$(document).ready(function () {
+    $('body').append(mytemplate({name: 'Harrison'}));
+
+    console.log( 'test complete' );
+});
+
